052afbc57c18c3081a00c9bc21bc9bc88faee26e
Zakhar <zakharkostyshyn@gmail.com>
2018-07-06 16:23:45 +0300

    FXML and Controller for calls registration application

---
+++ Curler_System/src/application/calls_registration/RegistrationController.java
@@ -0,0 +1,26 @@
+package application.calls_registration;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class RegistrationController {
+	    @FXML
+	    private TextField startadress_field;
+
+	    @FXML
+	    private TextField phone_field;
+
+	    @FXML
+	    private TextField endadress_field;
+
+	    @FXML
+	    private TextField fullname_field;
+
+	    @FXML
+	    private Button registrate_button;
+	    
+	    @FXML
+	    private Button back_button; 
+
+}
